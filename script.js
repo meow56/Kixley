d8bce733e2fc5ea1759584f219afa3bf62ab2317
Colin Pulis <colinpulis@gmail.com>
2018-08-17 11:45:02 -0500

    Maybe now I fixed it?

--- script.js
+++ script.js
@@ -423,8 +423,8 @@ function Fight(faction1, faction2) { // faction 1: [faction name, kixley, fighte
           actualSpec(this.notTurn[this.target[i]]);
           break;
         case "Special Attack":
-          alert('This class doesn\'t have a special attack.')
-          Fight()
+          writeText('This class doesn\'t have a special attack.')
+          fightLoop()
           break;
         default:
           const err = new Error("\"" + this.action[i] + "\" is not a valid command.");
