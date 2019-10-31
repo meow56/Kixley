4753735aa0f4e8b9a6904e5eccbaf23aa6d5f8fe
meow56 <spinattacktotheface@gmail.com>
2019-10-31 16:38:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //alert("Ok, what the heck.");
 
-export function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
+export randomNumber;
 import { kixleyNCo, fightHandler } from './fight.js';
 
 alert("Uh oh...");
