af68ab71b0721ffb41195c221d10d3fbdf94fc4d
meow56 <spinattacktotheface@gmail.com>
2019-10-31 13:41:40 -0500

    Update fight.js

--- fight.js
+++ fight.js
@@ -1,4 +1,4 @@
-import randomNumber from './main.js';
+import { randomNumber } from './main.js';
 
 window.onerror = function(message, source, lineno, colno, error) {
   if(error.message === "Thanks for playing!") {
