bf3f1d74cfb96d60e5dee4cb6e884541426b3057
meow56 <spinattacktotheface@gmail.com>
2019-10-31 20:15:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -100,11 +100,8 @@ var theWholeShebang = [
   'And Atlassian, for making Bitbucket, which was used to create this.'
 ]; // Credits!
 // menu
-var openingMenu;
 var chosenClass;
-var loc;
 var volumeSettings = '10';
-var from;
 // items
 // accounts
 var userCheck;
@@ -137,53 +134,10 @@ var possibleItems = [
 ];
 var reward; // how much gold/exp you get when you finish a quest
 // music
-var fightMusic;
-var towerMusic;
-var placesMusic;
-var menuMusic;
-var townMusic;
-var innMusic;
-var marketplaceMusic;
-var gameOverMusic;
-var endMusic;
 
 /*******************\
 |      UTILITY      |
 \*******************/
-
-function playMusic(which) { // in the form of the variable ie fightMusic, placesMusic, etc.
-  fightMusic.pause();
-  fightMusic.currentTime = 0;
-  towerMusic.pause();
-  towerMusic.currentTime = 0;
-  placesMusic.pause();
-  placesMusic.currentTime = 0;
-  menuMusic.pause();
-  menuMusic.currentTime = 0;
-  townMusic.pause();
-  townMusic.currentTime = 0;
-  innMusic.pause();
-  innMusic.currentTime = 0;
-  marketplaceMusic.pause();
-  marketplaceMusic.currentTime = 0;
-  gameOverMusic.pause();
-  gameOverMusic.currentTime = 0;
-  endMusic.pause();
-  endMusic.currentTime = 0;
-  which.play();
-}
-
-function loadMusic() {
-  fightMusic = document.getElementById('FightMusic');
-  towerMusic = document.getElementById('TowerMusic');
-  placesMusic = document.getElementById('MarketplaceMusic');
-  menuMusic = document.getElementById('MenuMusic');
-  townMusic = document.getElementById('MarketplaceMusic');
-  innMusic = document.getElementById('MarketplaceMusic');
-  marketplaceMusic = document.getElementById('MarketplaceMusic');
-  gameOverMusic = document.getElementById('GameOverMusic');
-  endMusic = document.getElementById('EndMusic');
-}
 
 function parseBool(stringBool) {
   if (stringBool === 'true') {
