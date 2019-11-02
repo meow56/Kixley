3d68f33006ac64d13e59b07c3ad2a1b5ba2968c3
meow56 <spinattacktotheface@gmail.com>
2019-11-01 21:25:15 -0500

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,7 +1,7 @@
 import { loadMusic, playMusic } from './music.js';
 import { initializeUsers, MakeNewAccount, login } from './users.js';
 import { fightHandler } from './fight.js';
-import { diffSetting } from './classes.js';
+import { diffSetting, Difficulty } from './classes.js';
 import { displayInventory } from './items.js';
 
 export { randomNumber, percentChance, StartUpMenu, requestNumber, requestInput, writeTextWait, writeText };
